99f900d04bcb7fdcc94af11543fa0f10e15c25b5
mahdimorasmmb <67733580+mahdimorasmmb@users.noreply.github.com>
2021-08-21 17:32:18 +0430

    refactor disply method and search v2

--- app.js
+++ app.js
@@ -1,46 +1,60 @@
-
+let postes = []
 document.addEventListener("DOMContentLoaded", () => {
-    const divpostes = document.querySelector('#postes')
+    const inputSearch = document.querySelector('#inputSearch');
     const showButton = document.querySelector('#show')
     const deletButton = document.querySelector("#clear")
-    listpost(divpostes)
+    listpost()
     deletButton.addEventListener("click", () =>{
         divpostes.innerHTML = "No items"
-        console.log(divpostes)
 
     })
     showButton.addEventListener("click", () =>{
-        listpost(divpostes)
+        listpost()
     })
+    
 
+})
+"ggdf".includes
+
+
+document.addEventListener("keyup",(event) =>{
+    let newPost=[]
+    newPost =postes.filter((item) =>{
+        return item.title.includes(event.target.value)
+    })
+    display(newPost)
 })
 
 
 
-const listpost = (divpostes) => {
-    divpostes.innerHTML=""
-    let postes = []
+const listpost = () => {
     fetch("https://jsonplaceholder.typicode.com/posts")
         .then((response) => {
             return response.json();
         })
         .then((data) => {
             postes = data
-            postes.map((post) => {
-                const div = document.createElement("div");
-                const h2 = document.createElement("h2");
-                const p = document.createElement("p");
-                div.classList.add("post")
-                h2.classList.add('title');
-                p.classList.add("description")
-                h2.textContent = post.title;
-                p.textContent = post.body;
-                div.appendChild(h2);
-                div.appendChild(p);
-                divpostes.appendChild(div);
-
-
-            })
+            display(postes)
         })
 }
+
+const display = (postes) => {
+    const divpostes = document.querySelector('#postes');
+    divpostes.innerHTML=""
+    postes.map((post) => {
+        const div = document.createElement("div");
+        const h2 = document.createElement("h2");
+        const p = document.createElement("p");
+        div.classList.add("post")
+        h2.classList.add('title');
+        p.classList.add("description")
+        h2.textContent = post.title;
+        p.textContent = post.body;
+        div.appendChild(h2);
+        div.appendChild(p);
+        divpostes.appendChild(div);
+
+
+    })
+};
 
